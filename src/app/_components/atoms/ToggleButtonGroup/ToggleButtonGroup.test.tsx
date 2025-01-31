ce08fe7ea88682727a20a9c1cf66c14721ba127c
zakzakst <e1433104@gmai.com>
2025-01-31 12:07:37 +0900

    修正

--- src/app/_components/atoms/ToggleButtonGroup/ToggleButtonGroup.test.tsx
+++ src/app/_components/atoms/ToggleButtonGroup/ToggleButtonGroup.test.tsx
@@ -8,24 +8,50 @@ const renderComponent = (props: ToggleButtonGroupProps): RenderResult => {
   return render(<ToggleButtonGroup {...props} />);
 };
 
+const ToggleButtonItems = [
+  {
+    id: "button1",
+    label: "button1",
+  },
+  {
+    id: "button2",
+    label: "button2",
+    isDisabled: true,
+  },
+];
+
 describe("ToggleButtonGroup", () => {
+  /**
+   * Propsの確認
+   */
   it("ボタンの内容が反映される", () => {
     const { getAllByRole } = renderComponent({
-      items: [
-        {
-          id: "button1",
-          label: "button1",
-        },
-        {
-          id: "button2",
-          label: "button2",
-          isDisabled: true,
-        },
-      ],
+      items: ToggleButtonItems,
     });
     const toggleButtonEls = getAllByRole("radio");
     expect(toggleButtonEls[0].textContent).toBe("button1");
     expect(toggleButtonEls[1].textContent).toBe("button2");
     expect(toggleButtonEls[1]).toBeDisabled();
+  });
+
+  it("カスタムクラスが設定される", () => {
+    const { getByRole } = renderComponent({
+      items: ToggleButtonItems,
+      className: "custom-class",
+    });
+    const toggleButtonGroupEl = getByRole("radiogroup");
+    expect(toggleButtonGroupEl).toHaveClass("custom-class");
+  });
+
+  it("無効状態が設定される", () => {
+    const { getByRole, getAllByRole } = renderComponent({
+      items: ToggleButtonItems,
+      isDisabled: true,
+    });
+    const toggleButtonGroupEl = getByRole("radiogroup");
+    expect(toggleButtonGroupEl).toHaveAttribute("aria-disabled", "true");
+    for (const radio of getAllByRole("radio")) {
+      expect(radio).toBeDisabled();
+    }
   });
 });
