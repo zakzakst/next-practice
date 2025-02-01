3ce5c53a1b5db9dfbf2428f15290758b5616cfb7
zakzakst <e1433104@gmai.com>
2025-02-01 20:34:59 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-import { useState } from "react";
+import {
+  useState,
+  // useEffect,
+  // useRef,
+} from "react";
 import { useForm } from "react-hook-form";
 // import styles from "@/app/page.module.scss";
 import FileTrigger from "@/app/_components/atoms/FileTrigger";
+import InputTest from "@/app/_components/atoms/InputTest";
 
 type FormData = {
   name: string;
@@ -24,6 +29,14 @@ export default function Home() {
   const [data, setData] = useState<FormData>(DefaultFormData);
   console.log("errors: ", errors);
 
+  // const nameInputRef = useRef<HTMLInputElement | null>(null);
+
+  // useEffect(() => {
+  //   // console.log(register("name"));
+  //   // register("name").ref('current')
+  //   nameInputRef.current?.focus();
+  // }, [register]);
+
   return (
     <>
       <FileTrigger
@@ -39,7 +52,7 @@ export default function Home() {
         <h1 className="text-3xl font-bold underline px-1">Hello, Next.js!</h1>
       </div> */}
       <form onSubmit={handleSubmit((data) => setData(data))}>
-        <input
+        {/* <input
           type="text"
           id="name"
           placeholder="名前"
@@ -55,11 +68,28 @@ export default function Home() {
             },
           })}
           // maxLength={5}
-        />
+        /> */}
         <button type="submit" disabled={Object.keys(errors).length > 0}>
           Submit
         </button>
-        {errors.name?.message && <p>{errors.name?.message}</p>}
+        {/* {errors.name?.message && <p>{errors.name?.message}</p>} */}
+        <InputTest
+          id="name"
+          label="名前"
+          placeholder="例）Taro"
+          {...register("name", {
+            required: "名前を入力してください",
+            maxLength: {
+              value: 5,
+              message: "5文字以下で入力してください",
+            },
+            pattern: {
+              value: /^[A-Za-z]+$/i,
+              message: "アルファベットで入力してください",
+            },
+          })}
+          errMsg={errors.name?.message}
+        />
       </form>
       <div>{JSON.stringify(data)}</div>
     </>
