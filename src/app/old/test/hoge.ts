edb126b4f576f7719b2f5bcaf446a50f54024592
zakzakst <e1433104@gmai.com>
2025-02-17 19:25:39 +0900

    修正

--- src/app/old/test/hoge.ts
+++
@@ -1,4 +0,0 @@
-// 数値のリストの各値を 3 倍にする関数
-export function tripleEachNumber(numbers: number[]): number[] {
-  return numbers.map((n) => n * 3);
-}
---
+++ src/app/old/test/instructions.ts
@@ -0,0 +1,39 @@
+// const arr1 = new Array(0);
+// console.log(arr1);
+
+// const pets = [];
+// pets[pets.length] = "cat";
+
+// const SayHello = () => {
+//   console.log("Hello");
+// };
+// SayHello();
+
+// const arr2 = [1, 2, 3, 4];
+// const sum = arr2.reduce(function (a, b) {
+//   return a + b;
+// });
+// console.log(sum);
+
+// const arr3 = [1, 2, 3, 4].map((e) => {
+//   return e + 10;
+// });
+// console.log(arr3);
+
+// const dogs = ["Rex", "Lassie"];
+// for (let i = 0; i < dogs.length; i++) {
+//   console.log(dogs[i]);
+// }
+
+// const myTest = (num: number) => {
+//   if (num > 0) {
+//     console.log(num);
+//     return;
+//   } else {
+//     console.log(num);
+//   }
+// };
+// myTest(2);
+
+const arr4 = [1, 2, 3, 4];
+for (const item of arr4) console.log(item);
